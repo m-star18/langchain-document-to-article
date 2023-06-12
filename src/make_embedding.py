57aa83d934475a0fbeeb6e31755e4e450168090c
m-star18 <masutangoribura@gmail.com>
2023-06-12 19:12:56 +0900

    Add: load_document

--- src/make_embedding.py
+++ src/make_embedding.py
@@ -24,3 +24,36 @@ class MakeDocumentEmbedding:
         self.document_data = self.load_document()
         self.split_document_data = self.text_splitter()
         self.faiss_db = self.make_embeddings()
+    
+    def load_document(self):
+        """load_document _summary_
+
+        _extended_summary_
+
+        Returns
+        -------
+        _type_
+            _description_
+
+        Raises
+        ------
+        ValueError
+            _description_
+        """
+        # Load the document
+        if self.file_path.endswith(".pdf"):
+            # Load the PDF file (if the file is a URL, load the PDF file from the URL)
+            if self.file_path.startswith("http"):
+                loader = OnlinePDFLoader(self.file_path)
+            else:
+                loader = PyPDFLoader(self.file_path)
+        # Load the Word file
+        elif self.file_path.endswith(".docx"):
+            loader = UnstructuredWordDocumentLoader(self.file_path)
+        # Load the Markdown file
+        elif self.file_path.endswith(".md"):
+            loader = UnstructuredMarkdownLoader(self.file_path)
+        else:
+            raise ValueError("Please specify the path of the PDF file to be read.")
+        document_data = loader.load()
+        return document_data
